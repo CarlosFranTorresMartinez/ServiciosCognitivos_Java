efe70f368901086269bbc31b9f93f7e039e14e65
CarlosFranTorresMartinez <carlos.torres@vallegrande.edu.pe>
2020-09-13 19:15:25 -0500

    Frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Init from "./components/init";
+
+function App() {
+  return (
+    <div>
+      <Init></Init>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/traductor/index.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Row,
+  Col
+} from "react-bootstrap";
+import axios from "axios";
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: "",
+      model_id: "en-es",
+      translate: [],
+    };
+  }
+
+  onChangetext = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:8080/add", {
+        text: [this.state.text],
+        model_id: this.state.model_id,
+      })
+      .then((response) => {
+        this.setState({
+          text: this.state.text,
+          model_id: this.state.model_id,
+          translate: [...response.data.translations],
+        }); 
+        console.log(response.data.translations);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <Container fluid="md">
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text>Traductor</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                value={this.state.text}
+                type="text"
+                onChange={this.onChangetext}
+              />
+              <Button variant="primary" type="submit" on>
+                Traducir
+              </Button>
+            </InputGroup>
+            <Row>
+              {this.state.translate.map((data, index) => (
+                <Col key={index}>{data.translation}</Col>
+              ))}
+            </Row>
+          </Container>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Index;
